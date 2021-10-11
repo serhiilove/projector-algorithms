9c136666f98e343747c1f13cb6fad512a749e949
Serhii Kyslovets <serhii.love@gmail.com>
2021-10-11 11:49:20 +0300

    chore: add answer for task b

--- homework/hm-1/part-1.py
+++ homework/hm-1/part-1.py
@@ -1,4 +1,44 @@
 # a. for (i = n; i > 1; i /= 2)
 #  		db_call()
+#
+# підходить, зробимо порядку n/2 викликів, що меньше n^n
 
-# підходить, бо ми зробимо порядку n/2 викликів
+
+# b. for (i = 0; i * i < n; i++)
+# 		db_call()
+#
+# підходить, зробимо порядку n^2 викликів, що меньше n^n
+
+
+# c. for (i = 0; i < n; i++)
+# 	  	for (j = 0; j < 356; j++)
+# 		  db_call()
+#
+#
+
+
+# d. for (i = 1; i <= n * n - 10; i++)
+# 		for (j = 1; j <= i; j ++)
+# 			db_call()
+#
+# e.  for (i = 1; i <= n; i++)
+# 		for (j = 1; j <= i; j++)
+# 			for (k = 1; k <= j; k++)
+# 				db_call()
+#
+# f. for (i = 1; i <= n; i++)
+# 		for (j = 1; j < i; j *= 2)
+# 			db_call()
+#
+# g. for (i = 1; i <= n; i++)
+# 		for (j = 1; j <= n; j += i)
+# 			db_call()
+#
+# h. void compute(int n)
+# 	  if (n == 0) return;
+# 	  for (int i = 0; i < n; i++)
+# 	  	  db_call()
+# 	  compute(n/2)
+# 	  compute(n/2)
+
+
