f7176c82020b960f24c37f30c921d87485383fb2
Serhii Kyslovets <serhii.love@gmail.com>
2021-11-04 14:07:54 +0200

    chroe: complete firt part of solution

--- homework/hm-2/part-2/part_2_raskin_bobbins.py
+++ homework/hm-2/part-2/part_2_raskin_bobbins.py
@@ -1,11 +1,9 @@
 # HackerRank: Raskin Bobbins
 
-# m - dollars
-
-# n - flavors
-# c - flavors cost
-
 # t - trips
+# m - dollars
+# n - flavors
+# c - flavors cost []
 
 test_1 = '2\n4\n5\n1 4 5 3 2\n4\n4\n2 2 4 3'
 
@@ -14,14 +12,26 @@ def raskin_bobbins(lines: str) -> None:
     list_of_lines = lines.splitlines()
     t = list_of_lines[0]
 
-    for i in range(1, len(list_of_lines), 3):
-        m = list_of_lines[i]
-        n = list_of_lines[i + 1]
-        c = list_of_lines[i + 2].split()
+    for k in range(1, len(list_of_lines), 3):
+        m = int(list_of_lines[k])
+        n = int(list_of_lines[k + 1])
+        c = list_of_lines[k + 2].split()
 
         print(m, n, c)
 
-    # print('\n')  # n[i] n[j] - ice cream ids, n[i] < n[j]
+        c_sorted = c
+        c_sorted.sort()
+        id_i = 0
+        id_j = n - 1
+
+        for j, cj in reversed(list(enumerate(c_sorted))):
+            if int(cj) + int(c_sorted[id_i]) == m:
+                id_j = j
+                break
+            if int(cj) < m - int(c_sorted[id_i]):
+                id_i += 1
+
+        print('ids | values in sorted array: ', id_i, id_j, '|', c_sorted[id_i], c_sorted[id_j])
 
 
 raskin_bobbins(test_1)
