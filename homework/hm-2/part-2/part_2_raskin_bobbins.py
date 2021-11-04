eadd8e7d22a1e6bcecfd6347b5d56add6218d602
Serhii Kyslovets <serhii.love@gmail.com>
2021-11-04 21:34:38 +0200

    chore: finish solution for the raskin-bobbins

--- homework/hm-2/part-2/part_2_raskin_bobbins.py
+++ homework/hm-2/part-2/part_2_raskin_bobbins.py
@@ -10,28 +10,24 @@ test_1 = '2\n4\n5\n1 4 5 3 2\n4\n4\n2 2 4 3'
 
 def raskin_bobbins(lines: str) -> None:
     list_of_lines = lines.splitlines()
-    t = list_of_lines[0]
 
     for k in range(1, len(list_of_lines), 3):
         m = int(list_of_lines[k])
         n = int(list_of_lines[k + 1])
-        c = list_of_lines[k + 2].split()
+        c = list(map(int, (list_of_lines[k + 2].split())))
 
-        print(m, n, c)
-
-        c_sorted = c
-        c_sorted.sort()
+        c_sorted = sorted(enumerate(c), key=lambda i: i[1])
         id_i = 0
         id_j = n - 1
 
         for j, cj in reversed(list(enumerate(c_sorted))):
-            if int(cj) + int(c_sorted[id_i]) == m:
+            if cj[1] + c_sorted[id_i][1] == m:
                 id_j = j
                 break
-            if int(cj) < m - int(c_sorted[id_i]):
+            if cj[1] < m - c_sorted[id_i][1]:
                 id_i += 1
 
-        print('ids | values in sorted array: ', id_i, id_j, '|', c_sorted[id_i], c_sorted[id_j])
+        print(c_sorted[id_i][0] + 1, c_sorted[id_j][0] + 1)
 
 
 raskin_bobbins(test_1)
